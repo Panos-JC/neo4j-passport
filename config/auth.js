1bedb808b7c7099ea48f0119d9df539f2802da2d
isaacguerreir <isaacguerreirocom@gmail.com>
2016-11-01 21:43:52 -0400

    Create auth.js

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '325172697848445', // your App ID
+        'clientSecret'  : '4c160edebb24c73b5da93bdddd6fdcda', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+    }
+
+};
